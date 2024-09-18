7492a7f24dd0de308651617d8370d854ca7f000f
ValdVdC <143099903+ValdVdC@users.noreply.github.com>
2024-09-18 14:11:31 +0000

    blabla

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,15 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MainComponent } from './components/main/main.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
   ],
   imports: [
     BrowserModule,
